c36fac69eae31b0bf99d19d29b89215af5c228f4
glenn.schuetze <glenn.schuetze@dotdotdot.me>
2014-04-17 17:14:33 +0200

    async fetching of trackeEdges

--- grails-app/services/de/dfki/gs/service/simulation/SimulationThreadFrameworkService.groovy
+++ grails-app/services/de/dfki/gs/service/simulation/SimulationThreadFrameworkService.groovy
@@ -5,22 +5,38 @@ import de.dfki.gs.domain.GasolineStation
 import de.dfki.gs.domain.GasolineStationType
 import de.dfki.gs.domain.Simulation
 import de.dfki.gs.domain.SimulationRoute
+import de.dfki.gs.domain.Track
 import de.dfki.gs.domain.TrackEdge
+import de.dfki.gs.model.elements.Agent
 import de.dfki.gs.model.elements.CarAgent
+import de.dfki.gs.model.elements.EFillingStationAgent
 import de.dfki.gs.model.elements.EnergyConsumptionModel
 import de.dfki.gs.model.elements.ModelCar
 import de.dfki.gs.model.elements.RoutingPlan
 import de.dfki.gs.model.elements.results.CarAgentResult
+import de.dfki.gs.model.elements.results.EFillingStationAgentResult
+import de.dfki.gs.service.ExperimentDataService
 import de.dfki.gs.simulation.CarStatus
 import de.dfki.gs.simulation.SchedulerStatus
 import de.dfki.gs.simulation.SimulationObject
 
 import de.dfki.gs.simulation.SimulationThreadTask
+import de.dfki.gs.threadutils.NotifyingBlockingThreadPoolExecutor
+import de.dfki.gs.threadutils.ThreadPoolExecutorUtils
+import grails.async.Promise
+import grails.gorm.DetachedCriteria
+import org.hibernate.FetchMode
 
+import javax.persistence.FetchType
+
+import static grails.async.Promises.*
 
 import grails.transaction.Transactional
 
 import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentMap
+import java.util.concurrent.CyclicBarrier
+
 
 @Transactional
 class SimulationThreadFrameworkService {
@@ -35,6 +51,7 @@ class SimulationThreadFrameworkService {
     ConcurrentHashMap<String, SchedulerStatus> statusForSession = new ConcurrentHashMap<String, SchedulerStatus>()
 
     ConcurrentHashMap<String, Map<Long, CarAgent>> carAgentsForSession = new ConcurrentHashMap<String, Map<Long, CarAgent>>()
+    ConcurrentHashMap<String, Map<Long, EFillingStationAgent>> fillingStationAgentsForSession = new ConcurrentHashMap<String, Map<Long, EFillingStationAgent>>()
 
 
 
@@ -53,6 +70,9 @@ class SimulationThreadFrameworkService {
         Simulation simulation = Simulation.get( simulationId )
 
         List<GasolineStation> gasolineStations = GasolineStation.findAllBySimulation( simulation )
+
+
+
         Map<String, Double> gasolineStationFillingPortions = new HashMap<String, Double>()
         gasolineStationFillingPortions.put( GasolineStationType.AC_2_3KW.toString(), 2.3 );
         gasolineStationFillingPortions.put( GasolineStationType.AC_3_7KW.toString(), 3.7 );
@@ -60,6 +80,17 @@ class SimulationThreadFrameworkService {
         gasolineStationFillingPortions.put( GasolineStationType.AC_22KW.toString(), 22 );
         gasolineStationFillingPortions.put( GasolineStationType.AC_43KW.toString(), 43 );
         gasolineStationFillingPortions.put( GasolineStationType.DC_50KW.toString(), 50 );
+
+
+        ConcurrentMap<Long, EFillingStationAgent> fillingStationMap = new ConcurrentHashMap<Long, EFillingStationAgent>()
+
+        long mmm = System.currentTimeMillis();
+        for ( GasolineStation gasolineStation : gasolineStations ) {
+
+            fillingStationMap.put( gasolineStation.id, EFillingStationAgent.createFillingStationAgent( gasolineStation ) )
+
+        }
+        log.error( "filling station agents created in ${(System.currentTimeMillis()-mmm)} ms" )
 
 
         /**
@@ -72,28 +103,80 @@ class SimulationThreadFrameworkService {
 
         StringBuilder sb = new StringBuilder()
 
+        // TODO: async: @link ( "http://grails.org/doc/2.3.0.M1/guide/async.html#asyncGorm" )
+
+
+        long m1 = System.currentTimeMillis()
+
+
+        List<Promise> proms = new ArrayList<Promise>()
+
+        List<Long> ids = simulation.simulationRoutes*.id
+
+        log.error( "ids: ${ids}" )
+
+        ids.each { Long l ->
+            def promise = SimulationRoute.async.task {
+
+                log.error( "fetching for ${l}" )
+
+                def tes = TrackEdge.withCriteria {
+                    eq( "simulationRouteId", l)
+                }
+
+            }
+
+            proms.add( promise )
+        }
+
+        def hua = waitAll( proms )
+
+
+
+
+        log.error( "filled tracks in ${(System.currentTimeMillis()-m1)} ms" )
+        m1 = System.currentTimeMillis()
+
+        log.error( "...." )
+
+
+        def simRouteMap = [ : ]
+        hua.flatten().each { TrackEdge te ->
+
+            def l = simRouteMap.get( te.simulationRouteId )
+
+            if ( l ) {
+                l << te
+            } else {
+                def newL = []
+                newL << te
+                simRouteMap.put( te.simulationRouteId, newL )
+            }
+
+        }
+
+        log.error( "filled track map in ${(System.currentTimeMillis()-m1)} ms" )
+        m1 = System.currentTimeMillis()
+
         for ( SimulationRoute simulationRoute : simulation.simulationRoutes ) {
 
             if ( simulationRoute ) {
 
-                List<TrackEdge> edges = new ArrayList<TrackEdge>()
-                for ( TrackEdge trackEdge : simulationRoute.track.edges ) {
-                    if ( trackEdge ) {
-                        edges.add( trackEdge )
-                    }
-                }
+                // List<TrackEdge> edges = hua.flatten().findAll { TrackEdge te -> te.simulationRouteId == simulationRoute.id }
+
+                List<TrackEdge> edges = simRouteMap.get( simulationRoute.id )
 
                 RoutingPlan routingPlan = RoutingPlan.createRoutingPlan( edges );
 
                 CarType carType = simulationRoute.carType
 
-                // TODO: create with type! and energy küsdk
                 ModelCar modelCar = ModelCar.createModelCar( new EnergyConsumptionModel(), carType, 5, 20 );
 
                 CarAgent carAgent = CarAgent.createCarAgent(
                         routingPlan,
                         modelCar,
                         gasolineStationFillingPortions,
+                        fillingStationMap,
                         gasolineStations,
                         simulationId,
                         35
@@ -104,10 +187,15 @@ class SimulationThreadFrameworkService {
             }
 
         }
+        log.error( "loaded agents : ${(System.currentTimeMillis()-m1)} ms" )
+
         log.debug( "added ${threadMap.size()} tasks to simulation: ${sb.toString()}" )
 
 
         carAgentsForSession.put( sessionId, threadMap )
+        fillingStationAgentsForSession.put( sessionId, fillingStationMap )
+
+
         statusForSession.put( sessionId, SchedulerStatus.init )
 
         log.debug( "simulation framework is initiated for session: ${sessionId}" )
@@ -195,10 +283,24 @@ class SimulationThreadFrameworkService {
     def setScale2( Integer scaleValue, String sessionId ) {
 
         Map<Long, CarAgent> threadMap = carAgentsForSession.get( sessionId )
+        Map<Long, EFillingStationAgent> fillingStationAgentMap = fillingStationAgentsForSession.get( sessionId )
 
         if ( threadMap ) {
 
             for ( CarAgent task : threadMap.values() ) {
+
+                if ( task.status == SchedulerStatus.play || task.status == SchedulerStatus.pause ) {
+                    task.setInterval( scaleValue )
+                }
+            }
+
+        } else {
+            log.error( "no threads found for session: ${sessionId}" )
+        }
+
+        if ( fillingStationAgentMap ) {
+
+            for ( EFillingStationAgent task : fillingStationAgentMap.values() ) {
 
                 if ( task.status == SchedulerStatus.play || task.status == SchedulerStatus.pause ) {
                     task.setInterval( scaleValue )
@@ -235,15 +337,32 @@ class SimulationThreadFrameworkService {
 
     def runSimulation2( String sessionId ) {
 
+
+
+        log.error( "starting.." )
+
+        Map<Long, EFillingStationAgent> fillingStationsMap = fillingStationAgentsForSession.get( sessionId )
         Map<Long, CarAgent> threadMap = carAgentsForSession.get( sessionId )
 
-        if ( threadMap ) {
+        int barrierSize = fillingStationsMap.size() + threadMap.size();
+
+        // Barrier barrier = new ButterflyBarrier( barrierSize );
+        CyclicBarrier barrier = new CyclicBarrier( barrierSize )
+
+        int personalId = 0;
+        if ( fillingStationsMap ) {
             /**
              * stepping through all simulationTasks  and say start!
              */
-            for ( CarAgent task : threadMap.values() ) {
+            for ( EFillingStationAgent task : fillingStationsMap.values() ) {
+                task.setBarrier( barrier )
+                task.personalId = personalId
                 task.start()
+
+                personalId++;
             }
+
+
 
             statusForSession.put( sessionId, SchedulerStatus.play )
 
@@ -253,6 +372,56 @@ class SimulationThreadFrameworkService {
             log.error( "no threads found for session: ${sessionId}" )
         }
 
+        if ( threadMap ) {
+            /**
+             * stepping through all simulationTasks  and say start!
+             */
+            for ( CarAgent task : threadMap.values() ) {
+                task.setBarrier( barrier )
+                task.personalId = personalId
+                task.start()
+
+                personalId++;
+            }
+
+            statusForSession.put( sessionId, SchedulerStatus.play )
+
+            log.error( "simulation started for session: ${sessionId}" )
+
+        } else {
+            log.error( "no threads found for session: ${sessionId}" )
+        }
+
+        log.error( "checker.." )
+
+        boolean allRoutesFinished = false;
+
+        while ( !allRoutesFinished ) {
+
+            for ( CarAgent agent : threadMap.values() ) {
+
+                allRoutesFinished = (
+                    agent.getCarStatus().equals( CarStatus.MISSION_ACCOMBLISHED )||agent.getCarStatus().equals( CarStatus.WAITING_EMPTY )
+                )
+
+                if ( allRoutesFinished == false ) {
+                    break;
+                }
+
+            }
+
+
+            if ( allRoutesFinished == true ) {
+                // stop them all
+                for ( Agent agent : threadMap.values() ) {
+                    agent.cancel();
+                }
+                for ( Agent agent : fillingStationsMap.values() ) {
+                    agent.cancel();
+                }
+
+            }
+        }
 
 
     }
@@ -289,6 +458,22 @@ class SimulationThreadFrameworkService {
 
 
     def pauseSimulation2( String sessionId ) {
+
+        Map<Long, EFillingStationAgent> fillingStationMap = fillingStationAgentsForSession.get( sessionId )
+
+        if ( fillingStationMap ) {
+
+            for (  EFillingStationAgent task : fillingStationMap.values()  ) {
+                task.pause()
+            }
+
+            statusForSession.put( sessionId, SchedulerStatus.pause )
+
+            log.debug( "simulation paused for session: ${sessionId}" )
+
+        } else {
+            log.error( "no threads found for session: ${sessionId}" )
+        }
 
         Map<Long, CarAgent> threadMap = carAgentsForSession.get( sessionId )
 
@@ -337,6 +522,23 @@ class SimulationThreadFrameworkService {
 
 
     def proceedSimulation2( String sessionId ) {
+
+
+        Map<Long, EFillingStationAgent> fillingStationMap = fillingStationAgentsForSession.get( sessionId )
+
+        if ( fillingStationMap ) {
+
+            for ( EFillingStationAgent task : fillingStationMap.values()  ) {
+                task.proceed()
+            }
+
+            statusForSession.put( sessionId, SchedulerStatus.play )
+
+            log.debug( "simulation proceeded for session: ${sessionId}" )
+
+        } else {
+            log.error( "no threads found for session: ${sessionId}" )
+        }
 
         Map<Long, CarAgent> threadMap = carAgentsForSession.get( sessionId )
 
@@ -403,7 +605,10 @@ class SimulationThreadFrameworkService {
     def stopSimulation2( Long simulationId, String sessionId ) {
 
         Map<Long, CarAgent> threadMap = carAgentsForSession.get( sessionId )
+        Map<Long, EFillingStationAgent> fillingStationMap = fillingStationAgentsForSession.get( sessionId )
+
         List<CarAgentResult> carAgentResults = new ArrayList<CarAgentResult>()
+        List<EFillingStationAgentResult> fillingResults = new ArrayList<EFillingStationAgentResult>()
 
         Long experimentRunResultId = null
 
@@ -420,13 +625,29 @@ class SimulationThreadFrameworkService {
 
             log.debug( "simulation stopped for session: ${sessionId} and try to save results" )
 
+        } else {
+            log.error( "no threads found for session: ${sessionId}" )
+        }
 
-            experimentRunResultId = experimentDataService.saveExperimentResult( carAgentResults )
+        if ( fillingStationMap ) {
+
+            for (  EFillingStationAgent task : fillingStationMap.values()  ) {
+
+                fillingResults.add( task.geteFillingStationAgentResult() )
+                task.cancel()
+
+            }
+
+            statusForSession.put( sessionId, SchedulerStatus.stop )
+
+            log.debug( "simulation stopped for session: ${sessionId} and try to save results" )
 
 
         } else {
             log.error( "no threads found for session: ${sessionId}" )
         }
+
+        experimentRunResultId = experimentDataService.saveExperimentResult( carAgentResults, fillingResults )
 
         return experimentRunResultId
     }
@@ -526,25 +747,10 @@ class SimulationThreadFrameworkService {
     }
 
 
-    def getCarInfos2( long simulationRouteId, String sessionId ) {
-
-        Map<Long, CarAgent> threadMap = threadsForSession.get( sessionId )
-
-        if ( threadMap ) {
-
-            CarAgent task = threadMap.get( new Long( simulationRouteId ) )
-            return task.dto()
-
-        } else {
-            log.error( "no threads found for session: ${sessionId}" )
-        }
-
-
-
-    }
-
 
     def collectInfo( String sessionId ) {
+
+        def resultMap = [ : ]
 
         def l = []
 
@@ -556,6 +762,7 @@ class SimulationThreadFrameworkService {
             def m = [ : ]
             CarAgent carAgent = carAgentEntry.value
 
+            m.personalId = carAgent.personalId
 
             // put car name:
             m.carName = carAgent.modelCar.carName
@@ -577,7 +784,42 @@ class SimulationThreadFrameworkService {
             l << m
         }
 
-        return l;
+        def p = []
+
+        Map<Long, EFillingStationAgent> stationMap = fillingStationAgentsForSession.get( sessionId )
+
+        for ( Map.Entry<Long, EFillingStationAgent> stationEntry : stationMap ) {
+
+            def m = [ : ]
+
+            EFillingStationAgent agent = stationEntry.value
+
+            m.personalId = agent.personalId
+
+            m.status = agent.fillingStationStatus.toString()
+
+            p << m
+        }
+
+        resultMap.cars = l;
+        resultMap.stations = p;
+
+        return resultMap;
+    }
+
+    def getCarInfos2( long simulationRouteId, String sessionId ) {
+
+        Map<Long, CarAgent> carAgentMap = carAgentsForSession.get( sessionId )
+
+        if ( carAgentMap ) {
+
+            CarAgent task = carAgentMap.get( new Long( simulationRouteId ) )
+            return task.dto()
+
+        } else {
+            log.error( "no threads found for session ${sessionId}" )
+        }
+
     }
 
     def getCarInfos( long simulationRouteId, String sessionId ) {
