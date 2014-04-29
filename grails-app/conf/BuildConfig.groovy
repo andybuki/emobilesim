4e1f9ce92dba9b7452abd54faf55b5513a3e7849
glenn.schuetze <glenn.schuetze@dotdotdot.me>
2014-04-29 10:45:46 +0200

    added quartz plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -98,6 +98,9 @@ grails.project.dependency.resolution = {
         runtime ":database-migration:1.3.2"
 
         compile ":cache:1.1.1"
+
+        compile ":quartz:1.0.1"
+
         // compile ':cache-ehcache:1.0.0'
     }
 }
