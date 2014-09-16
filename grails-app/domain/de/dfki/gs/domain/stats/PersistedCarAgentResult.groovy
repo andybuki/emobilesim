552b2fc755a778fdfd5669ae89f37aecaf4acd6d
glenn.schuetze <glenn.schuetze@dotdotdot.me>
2014-09-16 13:57:07 +0200

    experiemnt data service ms2

--- grails-app/domain/de/dfki/gs/domain/stats/PersistedCarAgentResult.groovy
+++ grails-app/domain/de/dfki/gs/domain/stats/PersistedCarAgentResult.groovy
@@ -1,6 +1,29 @@
 package de.dfki.gs.domain.stats
 
+import de.dfki.gs.domain.simulation.CarType
+import de.dfki.gs.simulation.CarStatus
+
 class PersistedCarAgentResult {
+
+
+    Float     energyConsumed
+    CarType   carType
+    String    carStatus
+    Float     plannedDistance
+    Float     realDistance
+    Integer   targets
+
+    Long      timeForPlannedDistance
+    Long      timeForRealDistance
+    Long      timeForLoading
+    Long      timeForDetour
+
+    Float     energyLoaded
+
+    Integer   fillingStationsVisited
+
+    float     relativeSearchLimit
+
 
     static constraints = {
     }
