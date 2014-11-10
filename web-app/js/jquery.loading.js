3d96534cdae7fb6d8d9183f2b102c548ad8f9ec2
Andrey Bukhman <anbu02@berlin-202.b.dfki.de>
2014-11-10 11:02:09 +0100

    loading faster with javascript

--- web-app/js/jquery.loading.js
+++ web-app/js/jquery.loading.js
@@ -40,7 +40,7 @@
         $(document).ajaxComplete(function () {
             self.setTimeout(function (){
                 $(loadingElement).hide();
-            }, 4000);
+            }, 1000);
         });
     };
 
