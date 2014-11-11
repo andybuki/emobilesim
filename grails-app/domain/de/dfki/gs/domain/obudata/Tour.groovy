7aa29b498f8f18e6a742dca40143146aefe3d836
Andrey Bukhman <anbu02@berlin-164.b.dfki.de>
2014-11-11 09:49:56 +0100

    Some changes in obu domain

--- grails-app/domain/de/dfki/gs/domain/obudata/Tour.groovy
+++ grails-app/domain/de/dfki/gs/domain/obudata/Tour.groovy
@@ -2,8 +2,15 @@ package de.dfki.gs.domain.obudata
 
 class Tour {
 
+    String vin
+    String name
+    String ownerName
 
+    float fromLat
+    float fromLon
 
+    float toLat
+    float toLon
 
 
     static constraints = {
